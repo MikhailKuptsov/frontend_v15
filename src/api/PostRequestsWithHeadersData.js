60e72b3d23d2e004f17428908acd1ab31978c57c
mikhail <mikhail.kuptsov.03@mail.ru>
2025-06-03 20:33:09 +0300

    изменена функция PostRequestWithHeadersData и введена функция изменения пароля

--- src/api/PostRequestsWithHeadersData.js
+++ src/api/PostRequestsWithHeadersData.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const PostRequestsWithHeadersData = async (api_request_link, json_data, headers_data) => {
   try {
-    const response = await axios.post(api_request_link, json_data, { headers: headers_data });
+    // const response = await axios.post(api_request_link, json_data, { headers: headers_data });
+    const response = await axios({method:'post', url:api_request_link, data:json_data, headers: {'api-session-key': headers_data} });
     return { data: response.data, status: response.status };
   } catch (error) {
       return { 
--- src/components/Reuse/ChangePasswordModals.js
+++ src/components/Reuse/ChangePasswordModals.js
@@ -1,11 +1,19 @@
 import { useState } from 'react';
 import {Button, Modal, Nav, Form} from 'react-bootstrap'
 
+import { PostRequestsWithHeadersData } from '../../api/PostRequestsWithHeadersData';
+
+import { BaseUrl } from '../../constans/Main_api_url';
+import { Api_auth } from '../../constans/Auth_api_url';
+import ArrayToString from '../../api/api_url_connection';
+
 function ChangePasswordModals() {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const userData = JSON.parse(sessionStorage.getItem('user_data'));
 
   //куда сохраняются данные из формы
     const [dataPassword, setDataPasword] = useState({ old_password: "", new_password: "" });
@@ -14,11 +22,20 @@ function ChangePasswordModals() {
     const handleChange=(event)=>{
         setDataPasword({...dataPassword, [event.target.name]:event.target.value})
     }
+
     const handleSubmit= async(event)=>{
-        event.preventDefault();
-        console.log(dataPassword)
+      event.preventDefault();
+      // console.log(dataPassword)
+      alert("Пароль изменён")
+      const result = await PostRequestsWithHeadersData(ArrayToString([BaseUrl, Api_auth["update_password"]]), dataPassword ,userData.api_session_key)
+      if (result.error){
+        alert(`Ошибка выхода. Код ошибки:${result.error}`)
+        console.log("Post_запрос не выполнен")
+      }else{
+        console.log("Post_запрос выполнен")
         alert("Пароль изменён")
-    }
+      }
+      }
 
   return (
     <>
