75deba566224fd83e63d99b99aff9d802f1a77b4
mikhail <mikhail.kuptsov.03@mail.ru>
2025-06-04 23:03:52 +0300

    подгрузка данных о пользователях рабочая версия

--- src/api/GetRequest.js
+++ src/api/GetRequest.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 export const GetRequest = async (api_request_link, headers_data) => {
   try {
-    const response = await axios.get(api_request_link, { headers: headers_data });
+    // const response = await axios.get(api_request_link, { headers: headers_data });
+    const response=await axios({method:"get", url:api_request_link, headers: {'api-session-key': headers_data}})
     return { data: response.data, status: response.status };
   } catch (error) {
       return { 
---
+++ src/components/Personal/AdminPageUsers/AdminPageUsersFetcher.js
@@ -0,0 +1,46 @@
+import { useState, useEffect } from "react";
+
+//тестовые JSON-данные
+import UserFULLdata from "../../../test_data/AdminPage/UserFULLdata.json"
+
+//Функция объединения 
+import ArrayToString from "../../../api/api_url_connection";
+import { BaseUrl } from "../../../constans/Main_api_url";
+import { api_users } from "../../../constans/Users_api_url";
+
+import { GetRequest } from "../../../api/GetRequest";
+
+import { Alert } from "react-bootstrap";
+
+import AdminUserCombine from "./AdminUserCombine";
+
+import LoadingStuck from "../../Reuse/LoadingStuck";
+
+export default function AdminPageUsersFetcher(){
+    const [data, setData] = useState(null);
+
+    useEffect(()=>{
+        const fetchData = async()=>{
+            try{
+                const userData = JSON.parse(sessionStorage.getItem('user_data'));
+                const result = await GetRequest(ArrayToString([BaseUrl,api_users["get_all_users"]]), userData.api_session_key );
+                // console.log(ArrayToString([BaseUrl,api_users["get_all_users"]]))
+                setData(result.data)
+            }catch(err){
+                alert("ошибка выполнения",err)
+            }
+        }
+        fetchData();
+    },[])
+    const userData = JSON.parse(sessionStorage.getItem('user_data'));
+    const result = GetRequest(ArrayToString([BaseUrl,api_users["get_all_users"]]), userData.api_session_key );
+
+    const users_all_dat=data
+    if (users_all_dat != null){
+        return(
+            <AdminUserCombine users_all_data={users_all_dat}/>
+        )
+    }else{
+        return(<LoadingStuck/>)
+    }
+}
